43de14bd5207765cd38428a6ef6e630ff60b45d3
mxpaspa <mxpaspa@gmail.com>
2018-07-07 13:06:41 -0400

    put authentication method added

--- index.js
+++ index.js
@@ -100,5 +100,6 @@ var unifiedServer = function (req,res){
 // Define the request router
 var router = {
   'ping' : handlers.ping,
-  'users' : handlers.users
+  'users' : handlers.users,
+  'tokens' : handlers.tokens
 };
