6b013542d4d5ac8b8cdeb48db2f67b49cb209dcc
mxpaspa <mxpaspa@gmail.com>
2018-07-07 22:48:11 -0400

    beginning handlers for uptime checks

--- index.js
+++ index.js
@@ -101,5 +101,6 @@ var unifiedServer = function (req,res){
 var router = {
   'ping' : handlers.ping,
   'users' : handlers.users,
-  'tokens' : handlers.tokens
+  'tokens' : handlers.tokens,
+  'checks' : handlers.checks
 };
