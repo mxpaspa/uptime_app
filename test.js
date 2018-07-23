27241a89af7c8cf5a33556c467897a7e615d7ffb
mxpaspa <mxpaspa@gmail.com>
2018-07-22 21:18:39 -0400

    finished the log lib,compressing, and truncating log files

---
+++ test.js
@@ -0,0 +1,20 @@
+
+
+var server = require('./lib/server');
+var workers = require('./lib/workers');
+
+
+// declare the app
+
+var app = {};
+
+// init function
+app.init = function(){
+
+};
+
+// execute that the init function
+app.init();
+
+// export the app
+module.exports = app;
